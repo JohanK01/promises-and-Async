037855a6b7a910af0c084539b41448ab6263f9d6
Johan <klausenjohan23@gmail.com>
2022-01-17 11:49:03 +0100

    used fetch instead of xhr

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,13 @@
-let xhttp = new XMLHttpRequest;
-xhttp.open('GET', 'data/people.json');
-xhttp.responseType = 'json';
-xhttp.send();
-
-xhttp.onload = () => {
-    const data = xhttp.response;
-    console.log(data);
-
-    showData = () =>{
-        for (let i = 0; i < data.length; i++) {
-            let li = document.createElement('li');
-            li.textContent = data[i];
-            document.getElementById('content').appendChild(li);
-        }
+fetch('data/people.json')
+    .then(res => res.json())
+    .then(data => 
         
-    }
 
-}
+
+showData = () => {
+    for (let i = 0; i < data.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = data[i];
+        document.getElementById('content').appendChild(li);
+    }
+});
