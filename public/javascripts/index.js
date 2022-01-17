c4478e5278e6d8ca94a87c16d68afa1f9c3bfd8e
Johan <klausenjohan23@gmail.com>
2022-01-17 12:52:21 +0100

    changed for loop to foreach

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,18 @@
 fetch('data/people.json')
     .then(res => res.json())
-    .then(data => 
-        
+    .then(data => {
 
-
-showData = () => {
-    for (let i = 0; i < data.length; i++) {
-        let li = document.createElement('li');
-        li.textContent = data[i];
-        document.getElementById('content').appendChild(li);
-    }
-});
+            // for (let i = 0; i < data.length; i++) {
+            //     let li = document.createElement('li');
+            //     li.textContent = data[i];
+            //     document.getElementById('content').appendChild(li);
+            // }
+            data.forEach(element => {
+                let li = document.createElement('li');
+                li.textContent = element;
+                document.getElementById('content').appendChild(li);
+            });
+    })
+    .catch(error => {
+        console.log('not found', error)
+    })
